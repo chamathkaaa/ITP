e09b625375fbd9138652e5d0de963f0ebdd517b0
UviniC <uviniwijesinghe@gmail.com>
2019-08-09 19:25:09 +0530

    DBConnection class creation

--- src/Util/DBConnection.java
+++ src/Util/DBConnection.java
@@ -1,5 +1,17 @@
 package Util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/malpiyali","root","chamathka1998");
+		
+		return conn;
+	}
 }
